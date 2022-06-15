9f3a953ec15cdb909af2c09c16a8def6bfe10939
MpD <mariapauladelvalle4@gmail.com>
2022-06-14 18:32:17 -0600

    Login and register correct and everything else correct

--- app/src/main/java/com/example/apprestaurante/ui/restaurante/RestauranteFragment.kt
+++ app/src/main/java/com/example/apprestaurante/ui/restaurante/RestauranteFragment.kt
@@ -1,4 +1,4 @@
-package com.example.apprestaurante.ui.home
+package com.example.apprestaurante.ui.restaurante
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.apprestaurante.databinding.FragmentHomeBinding
+import com.example.apprestaurante.databinding.FragmentRestauranteBinding
+import com.example.apprestaurante.viewModel.RestauranteViewModel
 
-class HomeFragment : Fragment() {
+class RestauranteFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentRestauranteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val restauranteViewModel =
+            ViewModelProvider(this).get(RestauranteViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentRestauranteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textRestaurante
+        restauranteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/apprestaurante/viewModel/RestauranteViewModel.kt
+++ app/src/main/java/com/example/apprestaurante/viewModel/RestauranteViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.apprestaurante.ui.home
+package com.example.apprestaurante.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class RestauranteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is restaurante Fragment"
     }
     val text: LiveData<String> = _text
 }
